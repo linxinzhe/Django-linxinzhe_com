ac27ff111dd4fc9d9518101da336bc23a10fa9ea
LIN Xinzhe <linxinzhechn@163.com>
2016-07-22 11:24:50 +0800

    feature(database): add postgresql as backend database

--- linxinzhe_com/settings.py
+++ linxinzhe_com/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+ONLINE = True
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -26,7 +27,6 @@ SECRET_KEY = ')b!(&0a#%)jl)2z&v6e$mua$+h=wixltap=eq-sh=u7wi1#c1-'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,17 +72,35 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'linxinzhe_com.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if ONLINE:
+    name = os.getenv('POSTGRESQL_INSTANCE_NAME')
+    user = os.getenv('POSTGRESQL_USERNAME')
+    password = os.getenv('POSTGRESQL_PASSWORD')
+    host = os.getenv('POSTGRESQL_PORT_5432_TCP_ADDR')
+    port = os.getenv('POSTGRESQL_PORT_5432_TCP_PORT')
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': name,
+            'USER': user,
+            'PASSWORD': password,
+            'HOST': host,
+            'PORT': port,
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'ztev',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
@@ -102,7 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
@@ -115,7 +132,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/

b958875b93e65dd340a48ad939b205d012440fe9
LIN Xinzhe <linxinzhechn@163.com>
2016-07-25 14:40:44 +0800

    feature(test): add ci test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class ViewTest(TestCase):
+    def testIndexTemplate(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home/index.html")

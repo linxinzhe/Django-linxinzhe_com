5aa214e56d32440466532a85ed08e2a07682e1d2
LIN Xinzhe <linxinzhechn@foxmail.com>
2016-08-08 14:25:53 +0800

    fix(rango): fix lost migrations by droping all and recreate one

--- rango/models.py
+++ rango/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField(blank=True, unique=True, default="")
+    slug = models.SlugField(unique=True)
 
     class Meta:
         verbose_name_plural = "categories"

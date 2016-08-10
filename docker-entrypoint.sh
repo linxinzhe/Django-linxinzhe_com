970bd2bfeca7273e71f70bd12ed78d8f49d38919
LIN Xinzhe <linxinzhechn@foxmail.com>
2016-08-10 15:10:12 +0800

    feature(all): gather static in one place

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 sleep 5
 nginx
+python3 /www/manage.py collectstatic --noinput
 python3 /www/manage.py makemigrations --noinput
 python3 /www/manage.py migrate --noinput
 /usr/local/bin/gunicorn linxinzhe_com.wsgi:application -w 2 -b :8000

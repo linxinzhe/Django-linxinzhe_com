5c349936dba9ab490cb3fe78795c2584ac103f9b
LIN Xinzhe <linxinzhechn@foxmail.com>
2016-08-10 21:50:07 +0800

    feature(all): test new way to save secret key

--- linxinzhe_com/settings.py
+++ linxinzhe_com/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-from linxinzhe_com.secret import SECRET_KEY
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
